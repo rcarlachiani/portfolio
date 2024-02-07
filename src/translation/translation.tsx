50ed509957839aa3884fc4eadb04d19e6e8b4c44
Rodrigo Carlachiani <rcarlachiani@gmail.com>
2024-02-07 12:22:49 -0300

    Actualizo textos y otros cambios generales

--- src/translation/translation.tsx
+++ src/translation/translation.tsx
@@ -13,7 +13,7 @@ export const SubtitlesEN = {
 export const TextEN = {
     technologies: "Technologies used:",
     goToSite: "Go to site",
-    aboutMe: "In the early stages of my career as a developer, I worked at a creative agency called <span><b>Genosha</b></span>, in where I had the opportunity to collaborate on projects that presented significant technical and creative challenges. I contributed to the creation of components and websites for digital products and experiences alongside teams in planning, design, backend, and QA. We utilized agile methodologies to drive the process forward.<br><br>Currently, I am employed at the <span><b>Ministry of Finance of the Province of Buenos Aires</b></span>, working on the development of an internal web system using React.js and Django Rest. This system aims to enhance and streamline the financial, budgetary, and administrative management of municipalities in the Province of Buenos Aires."
+    aboutMe: "In the early stages of my career, I worked at a creative agency called  <span><b>Genosha</b></span>, where I had the opportunity to collaborate on projects that presented significant creative challenges.<br><br> Currently, I work for the <span><b>Ministry of Finance of the Province of Buenos Aires</b></span>, developing an internal web system using ReactJs, Python and Django Rest. The goal of this project is to optimize the management of municipalities in the province."
 }
 
 export const TitlesES = {
@@ -31,5 +31,5 @@ export const SubtitlesES = {
 export const TextES = {
     technologies: "Tecnologías usadas:",
     goToSite: "Ir al sitio",
-    aboutMe: "En los comienzos de mi carrera, trabajé en una agencia creativa llamada <span><b>Genosha</b></span>, donde he tenido la oportunidad de participar en proyectos que me han demandado grandes desafíos creativos y técnicos.<br><br>Actualmente, trabajo para el <span><b>Ministerio de Hacienda y Finanzas de la Prov. de Buenos Aires</b></span> en el desarrollo de un sistema web interno utilizando ReactJs y Python con Django REST Framework, el cual tiene como objetivo optimizar la gestión de los municipios de la provincia."
+    aboutMe: "En los comienzos de mi carrera, trabajé en una agencia creativa llamada <span><b>Genosha</b></span>, en donde he tenido la oportunidad de colaborar en proyectos que me han demandado grandes desafíos creativos.<br><br>Actualmente, trabajo para el <span><b>Ministerio de Hacienda y Finanzas de la Prov. de Buenos Aires</b></span> en el desarrollo de un sistema web interno utilizando ReactJs, Python y Django Rest, el cual tiene como objetivo oprtimizar la gestión de los municipios."
 }
