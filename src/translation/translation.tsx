0114b1ec10ff496b22b89a7ca20165bf206ac679
Rodrigo Carlachiani <rcarlachiani@gmail.com>
2024-07-10 12:28:12 -0300

    Text correction

--- src/translation/translation.tsx
+++ src/translation/translation.tsx
@@ -7,55 +7,61 @@ export const TitlesEN = {
 }
 
 export const SubtitlesEN = {
-    whoIAm: "I'm Rodrigo",
-    role: "{ Frontend Developer }",
-    experience: {
-        first: "Jan. 2022 - May. 2023 | Trainee Fronted Dev - Jr. Frontend Dev",
-        second: "Aug. 2023 - Present | Jr. Fullstack Dev"
-    }
-}
+  whoIAm: "I'm Rodrigo",
+  role: '{ Software Developer }',
+  experience: {
+    first: 'Jan. 2022 - May. 2023 | Frontend Developer',
+    second: 'Aug. 2023 - Present | Fullstack Developer',
+  },
+};
 
 export const TextEN = {
-    technologies: "Technologies used:",
-    goToSite: "Go to site",
-    experience: {
-        first: "In the early stages of my career, I worked at a creative agency called <span><b>Genosha</b></span>, initially as a Trainee and later as a Junior. During my time there, I had the opportunity to engage in projects that posed significant creative and technical challenges. I specialized in developing the Frontend of web applications, primarily using ReactJS and NextJS, along with various frameworks and integrations with REST APIs.",
-        second: "Currently, I work as a Junior Fullstack Developer for the <span><b>Ministry of Economy and Finance of the Province of Buenos Aires</b></span>, involved in the development of an internal web system using ReactJS, Python, and Django REST Framework. The goal of this project is to optimize the management of resources for municipalities."
-    },
-    aboutMe: "My journey as a developer began in 2021, when I started my formal studies in the Web Development career with ReactJS at Coderhouse, graduating in 2022.<br> In that year, I secured my first internship as a layout designer, and since then, I have been actively working as a Frontend Developer, continuing to this day with my ongoing education and growth through constant study and practice.<br> Currently, I am studying Python, also at Coderhouse, with the goal of becoming a fullstack developer.<br><br> Beyond development, I have a deep passion for music; I am a drummer, sound technician, and owner of a diverse vinyl collection, a true reflection of my musical tastes."
-}
+  technologies: 'Technologies used:',
+  goToSite: 'Go to site',
+  experience: {
+    first:
+      'In the early stages of my career, I worked at a creative agency called <span><b>Genosha</b></span>, where I had the opportunity to engage in projects that posed significant creative and technical challenges. I specialized in developing the Frontend of web applications, primarily using ReactJS and NextJS, along with various frameworks and integrations with REST APIs.',
+    second:
+      'Currently, I work as a Fullstack Developer for the <span><b>Ministry of Economy and Finance of the Province of Buenos Aires</b></span>, involved in the development of an internal web system using ReactJS, Python, and Django REST Framework. The goal of this project is to optimize the management of resources for municipalities.',
+  },
+  aboutMe:
+    'My journey as a developer began in 2021, when I started my formal studies in the Web Development career with ReactJS at Coderhouse, graduating in 2022.<br> In that year, I secured my first internship as a layout designer, and since then, I have been actively working as a Frontend Developer, continuing to this day with my ongoing education and growth through constant study and practice.<br> In 2024, driven by this spirit, I took a Development course in Python and Django, with the goal of improving and expanding my skills.<br><br> Beyond development, I have a deep passion for music; I am a drummer, sound technician, and owner of a wide vinyl collection.',
+};
 
 export const ButtonsEN = {
-    downloadCV: "Download Resume",
-    goToProject: "Go to site"
-}
+  downloadCV: 'Download Resume',
+  goToProject: 'Go to site',
+};
 
 export const TitlesES = {
-    greeting: "¡Hola!",
-    about: "<span> Acerca</span> de mí",
-    skills: "<span>Tecnologías</span> con\n las que trabajo",
-    works: "<span>Proyectos</span>\n que desarrollé",
-    experience: "<span>Experiencia</span> profesional"
-}
+  greeting: '¡Hola!',
+  about: '<span> Acerca</span> de mí',
+  skills: '<span>Tecnologías</span> con\n las que trabajo',
+  works: '<span>Proyectos</span>\n que desarrollé',
+  experience: '<span>Experiencia</span> profesional',
+};
 
 export const SubtitlesES = {
-    whoIAm: "Soy Rodrigo",
-    role: "{ Desarrollador Frontend }",
-    experience: {
-        first: "Ene. 2022 - May. 2023 | Trainee Fronted Dev - Jr. Frontend Dev",
-        second: "Ago. 2023 - Present | Jr. Fullstack Dev"
-    }
-}
+  whoIAm: 'Soy Rodrigo',
+  role: '{ Software Developer }',
+  experience: {
+    first: 'Ene. 2022 - May. 2023 | Desarrollador Frontend',
+    second: 'Ago. 2023 - Present | Desarrollador Fullstack',
+  },
+};
 
 export const TextES = {
-    technologies: "Tecnologías usadas:",
-    goToSite: "Ir al sitio",
-    experience: {
-        first: "En los comienzos de mi carrera, trabajé en una agencia creativa llamada <span><b>Genosha</b></span>, primero como Trainee y luego como Junior, en donde he tenido la oportunidad de participar en proyectos que me han demandado grandes desafíos creativos y técnicos, desarrollando el Frontend de aplicaciones web principalmente con ReactJs y NextJs, diversos frameworks e integraciones con APIs REST.",
-        second: "Actualmente, me desempeño como Desarrollador Fullstack Junior para el <span><b>Ministerio de Hacienda y Finanzas de la Prov. de Buenos Aires</b></span> en el desarrollo de un sistema web de uso interno utilizando ReactJs, Python y Django REST Framwork, el cual tiene como objetivo oprtimizar la gestión de los recursos de los municipios."
-    },
-    aboutMe: "Mi camino como desarrollador comienza en 2021, año en el que ingresé en la carrera de Desarrollo Web con ReactJS de Coderhouse, de la cual me gradué en 2022.<br> Ése año, conseguí mi primer pasantía como maquetador y desde entonces, he estado trabajando de manera activa como Desarrollador Frontend, continuando con mi formación y crecimiento a través del estudio y la práctica constante.<br> Actualmente estoy estudiando Python, también en Coderhouse, con la meta de convertirme en desarrollador fullstack.<br><br> Además del desarrollo, tengo una gran pasión por la música; soy baterista, técnico de sonido y poseedor de una variada colección de vinilos, fiel reflejo de mis gustos musicales."
-}
+  technologies: 'Tecnologías usadas:',
+  goToSite: 'Ir al sitio',
+  experience: {
+    first:
+      'En los comienzos de mi carrera, trabajé en una agencia creativa llamada <span><b>Genosha</b></span>, en donde he tenido la oportunidad de participar en proyectos que me han demandado grandes desafíos creativos y técnicos, desarrollando el Frontend de aplicaciones web principalmente con ReactJs y NextJs, diversos frameworks e integraciones con APIs REST.',
+    second:
+      'Actualmente, me desempeño como Desarrollador Fullstack para el <span><b>Ministerio de Hacienda y Finanzas de la Prov. de Buenos Aires</b></span> en la creación de un sistema web de uso interno utilizando ReactJs, Python y Django REST Framwork, el cual tiene como objetivo oprtimizar la gestión de los recursos de los municipios.',
+  },
+  aboutMe:
+    'Mi camino como desarrollador comienza en 2021, año en el que ingresé en la carrera de Desarrollo Web con ReactJS de Coderhouse, de la cual me gradué en 2022.<br> Ése año, conseguí mi primer pasantía como maquetador y desde entonces, he estado trabajando de manera activa como Desarrollador Frontend, continuando con mi formación y crecimiento a través del estudio y la práctica constante.<br> En 2024, impulsado por éste espíritu, realicé un curso de Desarrollo en Python con Django, con la meta de mejorar y ampliar mis skills.<br><br> Además del desarrollo, tengo una gran pasión por la música; soy baterista, técnico de sonido y poseedor de una vasta colección de vinilos.',
+};
 
 export const ButtonsES = {
     downloadCV: "Descargar CV",
